a6ad58a8e12758b19f5cbf419a1710166be07836
Caroline DINOUARD <caroline.dinouard@gmail.com>
2021-02-06 12:21:15 +0100

    ajout php formulaire de contact

---
+++ index.php
@@ -0,0 +1,256 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylesheet' href='main.css'>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap" rel="stylesheet">
+    <title>CV Caroline Dinouard</title>
+</head>
+<body>
+
+<!-- NAVIGATION ET LOGO -->
+    <header id="accueil"> 
+        <p class="site-name">Caroline Dinouard</p>   
+        <nav class="main-navigation">
+            <ul class="menu">
+                <li><a class="lien-menu" href="#accueil">Accueil</a></li>
+                <li><a class="lien-menu" href="#apropos">A propos</a></li>
+                <li><a class="lien-menu" href="#competences">Compétences</a></li>
+                <li><a class="lien-menu" href="#projets">Projets</a></li>
+                <li><a class="lien-menu" href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+        <div class="burger">
+            <div class="line1"></div>
+            <div class="line2"></div>
+            <div class="line3"></div>
+        </div>
+    </header>
+
+<!-- BANNIERE PRINCIPALE -->
+    <section class="main-banner">
+        <div class='banner-text'>
+            <h1>Développeuse Web Junior</h1>
+            <p>En formation développeur web et web mobile</p>
+            <div class='banner-btn'>
+                <a href="#"><button class="head-btn">Télécharger le CV</button></a>
+            </div>
+            <div class="social">
+                <a href="https://www.linkedin.com/in/carolinedinouard/"><i class="fa fa-linkedin"></i></a>
+            </div>
+        </div>
+    </section>
+
+<!-- A PROPOS -->
+    <section class="a-propos" id="apropos">
+        <div class="left-col">
+            <div class="res">
+                <h2 data-text="Résumé..." class="h2res">Résumé...</h2>
+            </div>
+            <div class="resume">
+                <p>Passionnée depuis mon adolescence par l'informatique, j'ai évolué au travers de mes études et expériences professionnelles, dans le domaine du webmarketing. Après 9 années dédiées au marketing digital, je prends un virage plus technique et souhaite rejoindre la grande famille du développement. Je me suis formée en ligne, en autodidacte au HTML, CSS, et JavaScript. En Mars 2021, j'intègre une formation au sein de l'école ENI pour étendre et valider mes compétences. Je souhaite à terme, évoluer dans des sociétés à impact positif.</p>
+            </div>
+        </div>
+        <div class="infos">
+            <ul>
+                <li><i class="fa fa-female"></i><strong>Profil MBTI : </strong>INFP</li>
+                <li><i class="fa fa-rocket"></i><strong>Déplacements : </strong>trottinette & voiture</li>
+                <li><i class="fa fa-camera"></i><strong>Passion : </strong>photographie</li>
+            </ul>
+        </div>
+    </section>
+
+<!-- COMPETENCES -->
+    <section class="skills" id="competences">
+        <div class="title-skill">
+            <h2 data-text="Compétences..." class="h2comp">Compétences...</h2>
+        </div>
+        <div class="p-skill">
+            <p>Les technologies que j'apprends</p>
+        </div>
+        <div class="container-cards">
+        <div class="container">
+            <div class="card">
+                <div class="circle">
+                    <h2>HTML5 / CSS3</h2>
+                </div>
+                <div class="content">
+                    <p>Apprentissage du langage HTML pour structurer les pages web, et du langage CSS afin de les styliser. J'utilise également la librairie Bootstrap.</p>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="card">
+                <div class="circle">
+                    <h2>JAVA / JEE</h2>
+                </div>
+                <div class="content">
+                    <p>J'apprends le langage de programmation orienté objet Java, ainsi que Java Enterprise Edition pour le développement d'applications.</p>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="card">
+                <div class="circle">
+                    <h2>PHP / SYMFONY</h2>
+                </div>
+                <div class="content">
+                    <p>Apprentissage du langage de programmation PHP, afin de construire des applications web. Utilisation du framework Symfony.</p>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="card">
+                <div class="circle">
+                    <h2>JavaScript / jQuery</h2>
+                </div>
+                <div class="content">
+                    <p>J'apprends le langage de script orienté objet, le JavaScript, pour créer des éléments complexes sur des pages web. Utilisation de la librairie jQuery.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+    <!-- PROJETS -->
+    <section class="projects" id="projets">
+        <div class="projects2">
+            <div class="left-cols">
+                <div class="projects-left-col">
+                    <a href="https://github.com/CarolineDinouard/" target="_blank" rel="noopener"><i class="fa fa-github fa-5x"></i></a>
+                </div>
+                <div class="projects-left-col-2">
+                    <a href="https://github.com/CarolineDinouard/" target="_blank" rel="noopener"><i class="fa fa-github-alt fa-5x"></i></a>
+                </div>
+            </div>
+            </div>
+            <div class="projects-right-col">
+                <div class="title-projects">
+                    <h2 data-text="Projets..." class="h2proj">Projets...</h2>
+                </div>
+                <div class="p-projects">
+                    <p>Que ce soit en autodidacte ou lors de ma formation, l'ensemble de mes projets développés dans différents langages de programmation sont disponibles sur mon dépôt GITHUB. Il est continuellement mis à jour, n'hésitez donc pas à revenir régulièrement. Cliquez sur l'image pour y accéder. <br><strong>Quelques exemples de projets :</strong> site internet d'un salon de coiffure, application météo JavaScript, un générateur de citations, etc.</p>
+                </div>
+            </div>
+            <div class="git-mobile">
+                <a href="https://github.com/CarolineDinouard/" target="_blank" rel="noopener"><i class="fa fa-github fa-5x"></i></a>
+            </div>
+    </section>
+
+    <?php
+    if(isset($_POST['submit'])){
+      $lastname = htmlspecialchars(stripslashes(trim($_POST['lastname'])));
+      $firstname = htmlspecialchars(stripslashes(trim($_POST['firstname'])));
+      $email = htmlspecialchars(stripslashes(trim($_POST['email'])));
+      $phone = htmlspecialchars(stripslashes(trim($_POST['phone'])));
+      $message = htmlspecialchars(stripslashes(trim($_POST['message'])));
+      if(!preg_match("/^[A-Za-z .'-]+$/", $lastname)){
+        $lastname_error = 'Entrez votre nom';
+      }
+      if(!preg_match("/^[A-Za-z .'-]+$/", $firstname)){
+        $firstname_error = 'Entrez votre prénom';
+      }
+      if(!preg_match("/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/", $email)){
+        $email_error = 'Email invalide';
+      }
+      if(!preg_match('/^[0-9]{10}+$/', $phone)){
+        $phone_error = 'Téléphone invalide';
+      }
+      if(strlen($message) === 0){
+        $message_error = 'Votre message ne doit pas être vide';
+      }
+    }
+  ?>
+
+<!-- CONTACT -->
+    <section class="contact-section" id="contact">
+        <div class="container-contact">
+            <div class="contactinfo">
+                <div>
+                    <h2>Contact</h2>
+                    <ul class="info">
+                        <li>
+                            <span><i class="fa fa-map-marker"></i></span>
+                            <span>Lattes (34)</span>
+                        </li>
+                        <li>
+                            <span><i class="fa fa-envelope"></i></span>
+                            <span><a class="nodec" href="mailto:caroline.dinouard@gmail.com">E-mail</a></span>
+                        </li>
+                        <li>
+                            <span><i class="fa fa-mobile"></i></span>
+                            <span><a class="nodec" href="tel:+33649451070">06.49.45.10.70</a></span>
+                        </li>
+                    </ul>
+                    <ul class="social-accounts">
+                        <li><a href="https://www.linkedin.com/in/carolinedinouard/" target="_blank" rel="noopener"><i class="fa fa-linkedin-square fa-2x"></i></a></li>
+                        <li><a href="https://www.instagram.com/kay_photographer_/" target="_blank" rel="noopener"><i class="fa fa-instagram fa-2x"></i></a></li>
+                        <li><a href="https://github.com/CarolineDinouard" target="_blank" rel="noopener"><i class="fa fa-github fa-2x"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="contactForm">
+                <h2>M'envoyer un message</h2>
+                <form action="#contact" method="POST">
+                    <div class="formBox">
+                        <div class="inputBox w50">
+                            <input type="text" id="lastname" name="lastname" required>
+                            <span>Nom</span>
+                            <p><?php if(isset($lastname_error)) echo $lastname_error; ?></p>
+                        </div>
+                        <div class="inputBox w50">
+                            <input type="text" id="firstname" name="firstname" required>
+                            <span>Prénom</span>
+                            <p><?php if(isset($firstname_error)) echo $firstname_error; ?></p>
+                        </div>
+                        <div class="inputBox w50">
+                            <input type="email" id="email" name="email" required>
+                            <span>E-mail</span>
+                            <p><?php if(isset($email_error)) echo $email_error; ?></p>
+                        </div>
+                        <div class="inputBox w50">
+                            <input type="tel" id="phone" name="phone" required>
+                            <span>Téléphone</span>
+                            <p><?php if(isset($phone_error)) echo $phone_error; ?></p>
+                        </div>
+                        <div class="inputBox w100">
+                            <textarea name="message" id="message" required></textarea>
+                            <span>Votre message</span>
+                            <p><?php if(isset($message_error)) echo $message_error; ?></p>
+                        </div>
+                        <div class="inputBox w100">
+                            <input type="submit" name="submit" value="Envoyer">
+                        </div>
+                    </div>
+                    <?php 
+                        if(isset($_POST['submit']) && !isset($lastname_error) && !isset($firstname_error) && !isset($email_error) && !isset($phone_error) && !isset($message_error)){
+                        $to = 'caroline.dinouard@gmail.com'; 
+                        $body = " Nom: $lastname\n Prénom: $firstname\n E-mail: $email\n Téléphone: $phone\n Message:\n $message";
+                        if(mail($to,'Message depuis le formulaire de contact CV', $body)){
+                            echo '<p style="color: orange">Message envoyé</p>';
+                        }else{
+                            echo '<p>Une erreur est survenue, merci de vérifier le formulaire.</p>';
+                        }
+                        }
+                    ?>
+                </form>
+            </div>
+        </div>
+
+    </section>
+
+    <!-- SECTION FOOTER -->
+    <footer>
+        <div class="footer">
+            &copy; <script>new Date().getFullYear()&&document.write(new Date().getFullYear());</script> - Caroline Dinouard
+        </div>
+    </footer>
+    <script src="app.js"></script>
+</body>
+</html>
